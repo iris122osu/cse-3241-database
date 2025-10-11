5dd3dadefa8557e4d62b619e8bf6634a445c15ad
iris122osu <moorman.122@osu.edu>
2025-10-11 19:33:33 -0400

    added database file parser and options print

--- src/main/DatabaseApp.java
+++ src/main/DatabaseApp.java
@@ -1,13 +1,45 @@
+import java.io.File;
+import java.util.Scanner;
+import database.Database;
+
 
 public final class DatabaseApp {
+    
+    private static Database database; 
 
     // no argument constructor
     private DatabaseApp() {
     }
 
+    private static void printOptions () {
+        System.out.println("Add: add a record");
+        System.out.println("Edit: Edit a record");
+        System.out.println("Search: search for a record");
+        System.out.println("Rent: register a rental");
+        System.out.println("Return: register a return");
+        System.out.println("Deliver: schedule a delivery");
+        System.out.println("Pickup: schedule a pickup");
+    }
 
+    public static Scanner scanner;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner userIn = new Scanner(System.in);
+
+        database = new Database();
+
+        database.ParseFromFile(new File("src\\main\\database\\data.txt"));
+        System.out.println("Data Loaded!");
+        String choice = "";
+        // while (!choice.equals("exit")) {
+        //     printOptions();
+
+        // }
+        
+
+
+        userIn.close();
+
+
     }
 
 }
---
+++ src/main/database/Database.java
@@ -0,0 +1,95 @@
+package database;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+
+// Object representation of 
+public class Database {
+
+    // The outer array list is for each row, each inner array is a row tuple
+    // Size variables describe how many columns are in a tuple (don't know if needed)
+    public ArrayList<String[]> Employees;
+    public final int EmployeeSize = 6;
+    public ArrayList<String[]> Warehouses;
+    public final int WarehouseSize = 8;
+    public ArrayList<String[]> Drones;
+    public final int DroneSize = 12;
+    public ArrayList<String[]> Equipment;
+    public final int EquipmentSize = 14;
+    public ArrayList<String[]> PurchaseOrders;
+    public final int PurchaseOrderSize = 8;
+    public ArrayList<String[]> Users;
+    public final int UserSize = 6;
+    public ArrayList<String[]> Damages;
+    public final int DamageSize = 6;
+    public ArrayList<String[]> Rentals;
+    public final int RentalSize = 6;
+    public ArrayList<String[]> Reviews;
+    public final int ReviewSize = 5;
+
+    public Database () {
+        Employees = new ArrayList<String[]>();
+        Warehouses = new ArrayList<String[]>();
+        Drones = new ArrayList<String[]>();
+        Equipment = new ArrayList<String[]>();
+        PurchaseOrders = new ArrayList<String[]>();
+        Users = new ArrayList<String[]>();
+        Damages = new ArrayList<String[]>();
+        Rentals = new ArrayList<String[]>();
+        Reviews = new ArrayList<String[]>();
+    }
+    
+
+    // This is expecting each line to say relation,att1,att2,att3,etc.
+    public void ParseFromFile(File file) {
+        try (Scanner fileData = new Scanner(file)) { 
+            while (fileData.hasNextLine()) {
+                String nextLine = fileData.nextLine();
+                System.out.println(nextLine);
+                if (nextLine.strip().length() > 0) {
+                    String[] values = nextLine.split(",");
+
+                    switch (values[0]) {
+                        case "employee":
+                            Employees.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "warehouse":
+                            Warehouses.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "drone":
+                            Drones.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "equipment":
+                            Equipment.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "purchaseorder":
+                            PurchaseOrders.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "user":
+                            Users.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "damage":
+                            Damages.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "rental":
+                            Rentals.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        case "review":
+                            Reviews.add(Arrays.copyOfRange(values, 1, values.length - 1));
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+                }
+            }
+            // System.out.println(Employees.get(0)[2]);
+            fileData.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
+    }
+
+}
