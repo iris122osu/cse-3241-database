00ee4a172052315854b40616264eb0ff476ab6cb
iris122osu <moorman.122@osu.edu>
2025-10-11 17:18:00 -0400

    Initial java app commit

---
+++ src/main/DatabaseApp.java
@@ -0,0 +1,13 @@
+
+public final class DatabaseApp {
+
+    // no argument constructor
+    private DatabaseApp() {
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
