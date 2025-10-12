0d214ade176b7e6735335563c13b2e4328c235cd
iris122osu <moorman.122@osu.edu>
2025-10-11 20:37:56 -0400

    changed database to use hashmap

--- src/main/database/Database.java
+++ src/main/database/Database.java
@@ -4,41 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
+import java.util.HashMap;
 
 // Object representation of 
 public class Database {
 
-    // The outer array list is for each row, each inner array is a row tuple
-    // Size variables describe how many columns are in a tuple (don't know if needed)
-    public ArrayList<String[]> Employees;
-    public final int EmployeeSize = 6;
-    public ArrayList<String[]> Warehouses;
-    public final int WarehouseSize = 8;
-    public ArrayList<String[]> Drones;
-    public final int DroneSize = 12;
-    public ArrayList<String[]> Equipment;
-    public final int EquipmentSize = 14;
-    public ArrayList<String[]> PurchaseOrders;
-    public final int PurchaseOrderSize = 8;
-    public ArrayList<String[]> Users;
-    public final int UserSize = 6;
-    public ArrayList<String[]> Damages;
-    public final int DamageSize = 6;
-    public ArrayList<String[]> Rentals;
-    public final int RentalSize = 6;
-    public ArrayList<String[]> Reviews;
-    public final int ReviewSize = 5;
+    public HashMap<String, ArrayList<String[]>> DataMap;
 
     public Database () {
-        Employees = new ArrayList<String[]>();
-        Warehouses = new ArrayList<String[]>();
-        Drones = new ArrayList<String[]>();
-        Equipment = new ArrayList<String[]>();
-        PurchaseOrders = new ArrayList<String[]>();
-        Users = new ArrayList<String[]>();
-        Damages = new ArrayList<String[]>();
-        Rentals = new ArrayList<String[]>();
-        Reviews = new ArrayList<String[]>();
+        DataMap = new HashMap<String, ArrayList<String[]>>();
+        
+        DataMap.put("employees", new ArrayList<String[]>());
+        DataMap.put("warehouses", new ArrayList<String[]>());
+        DataMap.put("drones", new ArrayList<String[]>());
+        DataMap.put("equipments", new ArrayList<String[]>());
+        DataMap.put("purchaseorders", new ArrayList<String[]>());
+        DataMap.put("users", new ArrayList<String[]>());
+        DataMap.put("damages", new ArrayList<String[]>());
+        DataMap.put("rentals", new ArrayList<String[]>());
+        DataMap.put("reviews", new ArrayList<String[]>());
     }
     
 
@@ -50,38 +34,8 @@ public class Database {
                 // System.out.println(nextLine);
                 if (nextLine.strip().length() > 0) {
                     String[] values = nextLine.split(",");
-
-                    switch (values[0]) {
-                        case "employee":
-                            Employees.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "warehouse":
-                            Warehouses.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "drone":
-                            Drones.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "equipment":
-                            Equipment.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "purchaseorder":
-                            PurchaseOrders.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "user":
-                            Users.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "damage":
-                            Damages.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "rental":
-                            Rentals.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        case "review":
-                            Reviews.add(Arrays.copyOfRange(values, 1, values.length - 1));
-                            break;
-                        default:
-                            throw new AssertionError();
-                    }
+                    // System.out.println(values[0]);
+                    DataMap.get(values[0]).add(Arrays.copyOfRange(values, 1, values.length - 1));
                 }
             }
             // System.out.println(Employees.get(0)[2]);
